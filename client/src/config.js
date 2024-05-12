6cf2b69a43eec2ed4997f20d5385057e7e3438f6
arvindchauhan1 <arvindchn1@gmail.com>
2024-05-13 01:12:12 +0530

    updated config

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,4 @@
-// export { BASE_URL };
-let BASE_URL = "http://localhost:4000/";
-
-const production = true;
-
-if (production) {
-  BASE_URL = "/";
-}
+// let BASE_URL = "http://localhost:4000/";
+const BASE_URL = "/";
 
 export { BASE_URL };
